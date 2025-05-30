bcfaffc089af1d5619b8371ed6d0712ed74d78bf
0xeno <117058013+0xeno@users.noreply.github.com>
2025-05-30 17:14:17 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from pages.home import Home
+from pages.prediksi import Prediction
+from pages.rekomendasi import Recommendation
+from utils.style import apply_custom_styles
+
+# Apply global CSS styling
+apply_custom_styles()
+
+# Sidebar for navigation
+st.sidebar.title("Navigasi")
+selection = st.sidebar.radio("Pilih Halaman:", ["Home", "Prediksi", "Rekomendasi"])
+
+# Page routing
+if selection == "Home":
+    page = Home()
+elif selection == "Prediksi":
+    page = Prediction()
+elif selection == "Rekomendasi":
+    page = Recommendation()
+
+page.render()
