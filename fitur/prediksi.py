b1ee2233b657b38080af5a4a5c8bc76b9af30e20
0xeno <117058013+0xeno@users.noreply.github.com>
2025-05-30 19:22:43 +0700

    Add files via upload

---
+++ fitur/prediksi.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import joblib
+
+class Prediction:
+    def __init__(self):
+        self.model = joblib.load("best_regression_model_20250529_131743.joblib")
+        
+    def render(self):
+        st.title("🔍 Prediksi Popularitas Lagu")
+        st.markdown("Masukkan fitur-fitur lagu di bawah ini:")
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            acousticness = st.number_input("Acousticness", min_value=0.0, max_value=1.0, step=0.01)
+            audio_mode = st.selectbox("Audio Mode",(0,1))
+            loudness = st.number_input("Loudness", min_value=-60.0, max_value=0.0, step=0.1)
+
+        with col2:
+            key = st.selectbox("key",('C','D♭','D','E♭','E','F','G♭','G','A♭','A','B♭','B'))
+            song_duration_ms = st.number_input("song duration", min_value=0.0, max_value=5.0, step=0.01)
+            audio_valence = st.number_input("Audio Valence", min_value=0.0, max_value=1.0, step=0.01)
+        model = Prediction()
+        if st.button("Prediksi"):
+            # Simulasi prediksi model
+            result = model.prediksi(acousticness,key,audio_mode,song_duration_ms,loudness,audio_valence)
+            result = int(round(result))
+            st.markdown(f"**Skor popularitas lagu anda adalah {result}**")
+            
+    def prediksi(self,acousticness, key, audio_mode, song_duration_ms, loudness, audio_valence):
+        # key preprocessing
+        key_mapping = {
+            'C': 0,
+            'D♭': 1,
+            'D': 2,
+            'E♭': 3,
+            'E': 4,
+            'F': 5,
+            'G♭': 6,
+            'G': 7,
+            'A♭': 8,
+            'A': 9,
+            'B♭': 10,
+            'B': 11
+        }
+        key_encoded = key_mapping[key]
+        song_duration_proses = song_duration_ms * 60000
+        features = [[
+        acousticness,
+        key_encoded,
+        audio_mode,
+        song_duration_proses,
+        loudness,
+        audio_valence
+        ]]
+        preidction = self.model.predict(features)
+        return preidction[0]
---
+++ fitur/rekomendasi.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+
+class Recommendation:
+    def render(self):
+        st.title("🎧 Refrensi Lagu")
+
+        df = pd.read_csv("data/Song_data_cleaned.csv")
+
+        st.markdown("### Filter Berdasarkan Karakteristik")
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            mood = st.selectbox("Mood Lagu", ["","Happy", "Chill", "Sad"])
+            production = st.selectbox("Tipe Produksi", ["","Acoustic", "Electronic", "Major Key Song", "Minor Key Song"])
+
+        with col2:
+            key = st.selectbox("Nada Dasar", ["", "C", "D", "E", "F", "G", "A", "B", "F#", "Bb", "Eb"])
+            volume = st.selectbox("Tingkat Volume", ["","Quiet", "Normal", "Loud"])
+        filtered_df = df.copy()
+        if st.button("Filter"):
+            # Filter mood
+            if mood == "Happy":
+                filtered_df = filtered_df[(filtered_df["audio_valence"] > 0.6) & (filtered_df["loudness"] > -10)]
+            elif mood == "Chill":
+                filtered_df = filtered_df[(filtered_df["audio_valence"] > 0.4) & (filtered_df["loudness"] <= -10)]
+            elif mood == "Sad":
+                filtered_df = filtered_df[filtered_df["audio_valence"] < 0.4]
+        
+            # Filer Production
+            if production == "Acoustic":
+                filtered_df = filtered_df[filtered_df['acousticness'] > 0.7]
+            elif production == "Electronic":
+                filtered_df = filtered_df[filtered_df['acousticness'] < 0.3]
+            elif production == "Major Key Song":
+                filtered_df = filtered_df[filtered_df['audio_mode'] == 1]
+            elif production == "Minor Key Song":
+                filtered_df = filtered_df[filtered_df['audio_mode'] == 0]
+
+            # Filter volume
+            if volume == "Quiet":
+                filtered_df = filtered_df[filtered_df["loudness"] < -20]
+            elif volume == "Normal":
+                filtered_df = filtered_df[(filtered_df["loudness"] >= -20) & (filtered_df["loudness"] <= -10)]
+            elif volume == "Loud":
+                filtered_df = filtered_df[filtered_df["loudness"] > -10]
+        
+            if key:
+                key_mapping = {"C": 0, "D": 2, "E": 4, "F": 5, "G": 7, "A": 9, "B": 11, "F#": 6, "Bb": 10, "Eb": 3}
+                filtered_df = filtered_df[filtered_df['key'] == key_mapping[key]]
+    
+        search = st.text_input("Search your song :")
+        if search:
+            search = search.title()
+            filtered_df = filtered_df[filtered_df['song_name'].str.contains(search, case=False, na=False)]
+
+        filtered_df = filtered_df.sort_values(by='song_popularity', ascending=False)
+
+        st.dataframe(filtered_df.head(5))
