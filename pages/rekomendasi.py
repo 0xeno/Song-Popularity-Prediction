fea0217e683ec9364e9efae6f1d2d48248df5fa0
0xeno <117058013+0xeno@users.noreply.github.com>
2025-05-30 17:36:38 +0700

    Update rekomendasi.py

--- pages/rekomendasi.py
+++ pages/rekomendasi.py
@@ -20,47 +20,45 @@ class Recommendation:
             volume = st.selectbox("Tingkat Volume", ["","Quiet", "Normal", "Loud"])
         with col3:
             key = st.selectbox("Nada Dasar", ["", "C", "D", "E", "F", "G", "A", "B", "F#", "Bb", "Eb"])
-
+        filtered_df = df.copy()
         if st.button("Filter"):
-            filtered_df = df.copy()
-
-        # Filter mood
-        if mood == "Happy":
-            filtered_df = filtered_df[(filtered_df["audio_valence"] > 0.6) & (filtered_df["loudness"] > -10)]
-        elif mood == "Chill":
-            filtered_df = filtered_df[(filtered_df["audio_valence"] > 0.4) & (filtered_df["loudness"] <= -10)]
-        elif mood == "Sad":
-            filtered_df = filtered_df[filtered_df["audio_valence"] < 0.4]
+            # Filter mood
+            if mood == "Happy":
+                filtered_df = filtered_df[(filtered_df["audio_valence"] > 0.6) & (filtered_df["loudness"] > -10)]
+            elif mood == "Chill":
+                filtered_df = filtered_df[(filtered_df["audio_valence"] > 0.4) & (filtered_df["loudness"] <= -10)]
+            elif mood == "Sad":
+                filtered_df = filtered_df[filtered_df["audio_valence"] < 0.4]
         
-        # Filer Production
-        if production == "Acoustic":
-            filtered_df = filtered_df[filtered_df['acousticness'] > 0.7]
-        elif production == "Electronic":
-            filtered_df = filtered_df[filtered_df['acousticness'] < 0.3]
-        elif production == "Major Key Song":
-            filtered_df = filtered_df[filtered_df['audio_mode_encoded'] == 1]
-        elif production == "Minor Key Song":
-            filtered_df = filtered_df[filtered_df['audio_mode_encoded'] == 0]
+            # Filer Production
+            if production == "Acoustic":
+                filtered_df = filtered_df[filtered_df['acousticness'] > 0.7]
+            elif production == "Electronic":
+                filtered_df = filtered_df[filtered_df['acousticness'] < 0.3]
+            elif production == "Major Key Song":
+                filtered_df = filtered_df[filtered_df['audio_mode_encoded'] == 1]
+            elif production == "Minor Key Song":
+                filtered_df = filtered_df[filtered_df['audio_mode_encoded'] == 0]
 
-        # Filter durasi
-        if duration == "Short":
-            filtered_df = filtered_df[filtered_df["song_duration_ms"] < 120000]
-        elif duration == "Average":
-            filtered_df = filtered_df[(filtered_df["song_duration_ms"] >= 120000) & (filtered_df["song_duration_ms"] <= 270000)]
-        elif duration == "Long":
-            filtered_df = filtered_df[filtered_df["song_duration_ms"] > 270000]
+            # Filter durasi
+            if duration == "Short":
+                filtered_df = filtered_df[filtered_df["song_duration_ms"] < 120000]
+            elif duration == "Average":
+                filtered_df = filtered_df[(filtered_df["song_duration_ms"] >= 120000) & (filtered_df["song_duration_ms"] <= 270000)]
+            elif duration == "Long":
+                filtered_df = filtered_df[filtered_df["song_duration_ms"] > 270000]
 
-        # Filter volume
-        if volume == "Quiet":
-            filtered_df = filtered_df[filtered_df["loudness"] < -20]
-        elif volume == "Normal":
-            filtered_df = filtered_df[(filtered_df["loudness"] >= -20) & (filtered_df["loudness"] <= -10)]
-        elif volume == "Loud":
-            filtered_df = filtered_df[filtered_df["loudness"] > -10]
+            # Filter volume
+            if volume == "Quiet":
+                filtered_df = filtered_df[filtered_df["loudness"] < -20]
+            elif volume == "Normal":
+                filtered_df = filtered_df[(filtered_df["loudness"] >= -20) & (filtered_df["loudness"] <= -10)]
+            elif volume == "Loud":
+                filtered_df = filtered_df[filtered_df["loudness"] > -10]
         
-        if key:
-            key_mapping = {"C": 0, "D": 2, "E": 4, "F": 5, "G": 7, "A": 9, "B": 11, "F#": 6, "Bb": 10, "Eb": 3}
-            filtered_df = filtered_df[filtered_df['key_encoded'] == key_mapping[key]]
+            if key:
+                key_mapping = {"C": 0, "D": 2, "E": 4, "F": 5, "G": 7, "A": 9, "B": 11, "F#": 6, "Bb": 10, "Eb": 3}
+                filtered_df = filtered_df[filtered_df['key_encoded'] == key_mapping[key]]
     
         search = st.text_input("Search your song :")
         if search:
