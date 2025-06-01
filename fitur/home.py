6e8c782eef0698380050d57eeb6fbe61caef60f4
0xeno <117058013+0xeno@users.noreply.github.com>
2025-06-01 11:52:46 +0700

    Update home.py

--- fitur/home.py
+++ fitur/home.py
@@ -12,10 +12,10 @@ class Home:
 
         with st.expander("Attribute Info"):
             st.markdown("""
-            - **acousticness** : A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic.  
-            - **audio mode** : Mode indicates the modality (major or minor) of a track. Major is represented by 1 and minor is 0.  
-            - **song duration** : The duration of the track in minute.  
-            - **key** : The key the track is in. Measure from 0 to 11  
+            - **acousticness** : A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic. 
+            - **key** : The key the track is in. Measure from C to B.
+            - **audio mode** : Mode indicates the modality (major or minor) of a track.   
+            - **song duration** : The duration of the track in minute.    
             - **loudness** : The overall loudness of a track in decibels (dB). Values typically range between -60 and 0 dB.
             - **audio valence** : A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track.  
             """)
